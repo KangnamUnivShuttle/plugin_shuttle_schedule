6696036595afcf34b080dc79a1f87ec8ff959b8d
Kim HyunWoo <stories2@naver.com>
2022-03-21 14:25:42 +0900

    add testcase

--- src/app.js
+++ src/app.js
@@ -12,12 +12,14 @@ app.post('/chat', (req, res) => {
     const successResponse = {
     }
 
+    console.log('body', req.body)
+
     const shuttleData = {
         '-LXcJzZkD2g9OG50q5pt': {
             name: '이공관 행',
             status: true,
             order: 1,
-            routes: {
+            stations: {
                 'Ah7lO0GS9M': {
                     name: '기흥역 4번출구',
                     status: true,
@@ -36,7 +38,7 @@ app.post('/chat', (req, res) => {
             name: '기흥역 행',
             status: true,
             order: 2,
-            routes: {
+            stations: {
                 'zbIclI1pzD': {
                     name: '이공관 (기흥역)',
                     status: true,
@@ -49,7 +51,7 @@ app.post('/chat', (req, res) => {
             name: '기흥역 행',
             status: true,
             order: 3,
-            routes: {
+            stations: {
                 'rIowOloH4l': {
                     name: '이공관 (사훈 프라자)',
                     status: true,
@@ -63,33 +65,38 @@ app.post('/chat', (req, res) => {
             status: true,
             order: 4,
             dev: true,
-            routes: {
+            stations: {
                 'Bunker1': {
                     name: '일연벙',
+                    descript: 'memo 1',
                     status: true,
                     order: 1,
                     schedule: ['09:00:00', '09:30:00', '20:40:00', '23:59:59']
                 },
                 'Bunker2': {
                     name: '이연벙',
+                    descript: 'memo 2',
                     status: false,
                     order: 2,
                     schedule: ['00:00:00']
                 },
                 'Bunker3': {
                     name: '삼연벙',
+                    descript: 'memo 3',
                     status: true,
                     order: 3,
                     schedule: ['09:00:00']
                 },
                 'Bunker4': {
                     name: '사연벙',
+                    descript: 'memo 4',
                     status: true,
                     order: 4,
                     schedule: ['00:00:00']
                 },
                 'Bunker5': {
                     name: '오연벙',
+                    descript: 'memo 5',
                     status: true,
                     order: 5,
                     schedule: []
@@ -101,7 +108,7 @@ app.post('/chat', (req, res) => {
             status: false,
             order: 5,
             dev: true,
-            routes: {
+            stations: {
                 'Hatchery': {
                     name: '해처리',
                     status: true,
@@ -111,6 +118,8 @@ app.post('/chat', (req, res) => {
             }
         }
     }
+
+    res.send(successResponse)
 })
 
 module.exports = app;
--- tests/app.spec.js
+++ tests/app.spec.js
@@ -7,20 +7,474 @@ let agent;
 let server;
 
 beforeAll((done) => {
-    server = http.createServer(app);
-    agent = request.agent(server);
-    done();
+  server = http.createServer(app);
+  agent = request.agent(server);
+  done();
 });
 
 afterAll((done) => {
-    server && server.close();
-    done();
+  server && server.close();
+  done();
 });
 
 describe("Test the root path", () => {
-    test("It should response the GET method", async () => {
-      const response = await agent.post('/chat');
-      expect(response.statusCode).toBe(200);
-      const data = response.body
-    });
+  test("Get available route list", async () => {
+    const response = await agent.post('/chat')
+      .type('application/json')
+      .send({
+        dev: true,
+        method: 'route',
+      })
+    expect(response.statusCode).toBe(200);
+    const data = response.body
+    expect(data).toBe({
+      "version": "2.0",
+      "template": {
+        "outputs": [
+          {
+            "carousel": {
+              "type": "basicCard",
+              "items": [
+                {
+                  "title": "셔틀 버스 행선지 선택",
+                  "description": "이동하려는 행선지를 선택하세요",
+                  "thumbnail": {
+                    "imageUrl": ""
+                  },
+                  "buttons": [
+                    {
+                      "action": "message",
+                      "label": "12시 방향",
+                      "messageText": "12시 방향"
+                    },
+                  ]
+                }
+              ]
+            }
+          }
+        ],
+        "quickReplies": [
+          {
+            "messageText": "홈 으로",
+            "action": "message",
+            "label": "홈"
+          },
+          {
+            "messageText": "뒤로 가기",
+            "action": "message",
+            "label": "↩"
+          }
+        ]
+      }
+    })
   });
+  test("Get available station list in not exist route", async () => {
+    const response = await agent.post('/chat')
+      .type('application/json')
+      .send({
+        dev: true,
+        method: 'stations',
+        route: 'Not exist route',
+      })
+    expect(response.statusCode).toBe(200);
+    const data = response.body
+    expect(data).toBe({
+      "version": "2.0",
+      "template": {
+        "outputs": [
+          {
+            "simpleText": {
+              "text": "존재하지 않는 경로 입니다."
+            }
+          }
+        ],
+        "quickReplies": [
+          {
+            "messageText": "홈 으로",
+            "action": "message",
+            "label": "홈"
+          },
+          {
+            "messageText": "뒤로 가기",
+            "action": "message",
+            "label": "↩"
+          }
+        ]
+      }
+    })
+  });
+  test("Get available station list in route", async () => {
+    const response = await agent.post('/chat')
+      .type('application/json')
+      .send({
+        dev: true,
+        method: 'stations',
+        route: 'Not enough minerals',
+      })
+    expect(response.statusCode).toBe(200);
+    const data = response.body
+    expect(data).toBe({
+      "version": "2.0",
+      "template": {
+        "outputs": [
+          {
+            "carousel": {
+              "type": "listCard",
+              "items": [
+                {
+                  "header": {
+                    "title": "12시 방향 행 경로"
+                  },
+                  "items": [
+                    {
+                      "title": "햄치즈",
+                      "description": "memo 1",
+                    },
+                    {
+                      "title": "삼연벙",
+                      "description": "memo 3",
+                    },
+                    {
+                      "title": "사연벙",
+                      "description": "memo 4",
+                    },
+                    {
+                      "title": "오연벙",
+                      "description": "memo 5",
+                    },
+                  ],
+                  "buttons": [
+                    {
+                      "label": "더보기",
+                      "action": "webLink",
+                      "webLinkUrl": "https://web.kangnam.ac.kr/menu/4990be9bdd4defbf92dde49a31ad1a3b.do"
+                    }
+                  ]
+                },
+              ]
+            }
+          }
+        ],
+        "quickReplies": [
+          {
+            "messageText": "홈 으로",
+            "action": "message",
+            "label": "홈"
+          },
+          {
+            "messageText": "뒤로 가기",
+            "action": "message",
+            "label": "↩"
+          }
+        ]
+      }
+    })
+  });
+  test("Should not return station list in disabled route", async () => {
+    const response = await agent.post('/chat')
+      .type('application/json')
+      .send({
+        dev: true,
+        method: 'stations',
+        route: 'Spawn more overlords',
+      })
+    expect(response.statusCode).toBe(200);
+    const data = response.body
+    expect(data).toBe({
+      "version": "2.0",
+      "template": {
+        "outputs": [
+          {
+            "simpleText": {
+              "text": "비활성화된 경로 입니다."
+            }
+          }
+        ],
+        "quickReplies": [
+          {
+            "messageText": "홈 으로",
+            "action": "message",
+            "label": "홈"
+          },
+          {
+            "messageText": "뒤로 가기",
+            "action": "message",
+            "label": "↩"
+          }
+        ]
+      }
+    })
+  });
+  test("Get nearest time in station", async () => {
+    const response = await agent.post('/chat')
+      .type('application/json')
+      .send({
+        dev: true,
+        method: 'neartime',
+        time: '09:20:00',
+        route: 'Not enough minerals',
+      })
+    expect(response.statusCode).toBe(200);
+    const data = response.body
+    expect(data).toBe({
+      "version": "2.0",
+      "template": {
+        "outputs": [
+          {
+            "carousel": {
+              "type": "listCard",
+              "items": [
+                {
+                  "header": {
+                    "title": "12시 방향 행 경로"
+                  },
+                  "items": [
+                    {
+                      "title": "일연벙",
+                      "description": "10분 후 출발 / 다음 20:40:00",
+                    },
+                    {
+                      "title": "삼연벙",
+                      "description": "운행종료 / 첫차 09:00:00",
+                    },
+                    {
+                      "title": "사연벙",
+                      "description": "운행종료 / 첫차 00:00:00",
+                    },
+                    {
+                      "title": "오연벙",
+                      "description": "운행 정보 없음",
+                    },
+                  ],
+                  "buttons": [
+                    {
+                      "label": "더보기",
+                      "action": "webLink",
+                      "webLinkUrl": "https://web.kangnam.ac.kr/menu/4990be9bdd4defbf92dde49a31ad1a3b.do"
+                    }
+                  ]
+                },
+              ]
+            }
+          }
+        ],
+        "quickReplies": [
+          {
+            "messageText": "홈 으로",
+            "action": "message",
+            "label": "홈"
+          },
+          {
+            "messageText": "뒤로 가기",
+            "action": "message",
+            "label": "↩"
+          }
+        ]
+      }
+    })
+  });
+  test("if station time == current time", async () => {
+    const response = await agent.post('/chat')
+      .type('application/json')
+      .send({
+        dev: true,
+        method: 'neartime',
+        time: '09:00:00',
+        route: 'Not enough minerals',
+      })
+    expect(response.statusCode).toBe(200);
+    const data = response.body
+    expect(data).toBe({
+      "version": "2.0",
+      "template": {
+        "outputs": [
+          {
+            "carousel": {
+              "type": "listCard",
+              "items": [
+                {
+                  "header": {
+                    "title": "12시 방향 행 경로"
+                  },
+                  "items": [
+                    {
+                      "title": "일연벙",
+                      "description": "잠시후 출발 / 다음 09:30:00",
+                    },
+                    {
+                      "title": "삼연벙",
+                      "description": "잠시후 출발 / 첫차 09:00:00",
+                    },
+                    {
+                      "title": "사연벙",
+                      "description": "운행종료 / 첫차 00:00:00",
+                    },
+                    {
+                      "title": "오연벙",
+                      "description": "운행 정보 없음",
+                    },
+                  ],
+                  "buttons": [
+                    {
+                      "label": "더보기",
+                      "action": "webLink",
+                      "webLinkUrl": "https://web.kangnam.ac.kr/menu/4990be9bdd4defbf92dde49a31ad1a3b.do"
+                    }
+                  ]
+                },
+              ]
+            }
+          }
+        ],
+        "quickReplies": [
+          {
+            "messageText": "홈 으로",
+            "action": "message",
+            "label": "홈"
+          },
+          {
+            "messageText": "뒤로 가기",
+            "action": "message",
+            "label": "↩"
+          }
+        ]
+      }
+    })
+  });
+  test("if station time passed", async () => {
+    const response = await agent.post('/chat')
+      .type('application/json')
+      .send({
+        dev: true,
+        method: 'neartime',
+        time: '09:01:00',
+        route: 'Not enough minerals',
+      })
+    expect(response.statusCode).toBe(200);
+    const data = response.body
+    expect(data).toBe({
+      "version": "2.0",
+      "template": {
+        "outputs": [
+          {
+            "carousel": {
+              "type": "listCard",
+              "items": [
+                {
+                  "header": {
+                    "title": "12시 방향 행 경로"
+                  },
+                  "items": [
+                    {
+                      "title": "일연벙",
+                      "description": "이번 09:30:00 / 다음 20:40:00",
+                    },
+                    {
+                      "title": "삼연벙",
+                      "description": "운행종료 / 첫차 09:00:00",
+                    },
+                    {
+                      "title": "사연벙",
+                      "description": "운행종료 / 첫차 00:00:00",
+                    },
+                    {
+                      "title": "오연벙",
+                      "description": "운행 정보 없음",
+                    },
+                  ],
+                  "buttons": [
+                    {
+                      "label": "더보기",
+                      "action": "webLink",
+                      "webLinkUrl": "https://web.kangnam.ac.kr/menu/4990be9bdd4defbf92dde49a31ad1a3b.do"
+                    }
+                  ]
+                },
+              ]
+            }
+          }
+        ],
+        "quickReplies": [
+          {
+            "messageText": "홈 으로",
+            "action": "message",
+            "label": "홈"
+          },
+          {
+            "messageText": "뒤로 가기",
+            "action": "message",
+            "label": "↩"
+          }
+        ]
+      }
+    })
+  });
+  test("if station time is O'oclock", async () => {
+    const response = await agent.post('/chat')
+      .type('application/json')
+      .send({
+        dev: true,
+        method: 'neartime',
+        time: '23:50:00',
+        route: 'Not enough minerals',
+      })
+    expect(response.statusCode).toBe(200);
+    const data = response.body
+    expect(data).toBe({
+      "version": "2.0",
+      "template": {
+        "outputs": [
+          {
+            "carousel": {
+              "type": "listCard",
+              "items": [
+                {
+                  "header": {
+                    "title": "12시 방향 행 경로"
+                  },
+                  "items": [
+                    {
+                      "title": "일연벙",
+                      "description": "9분 후 출발 / 첫차 09:00:00",
+                    },
+                    {
+                      "title": "삼연벙",
+                      "description": "운행종료 / 첫차 09:00:00",
+                    },
+                    {
+                      "title": "사연벙",
+                      "description": "운행종료 / 첫차 00:00:00",
+                    },
+                    {
+                      "title": "오연벙",
+                      "description": "운행 정보 없음",
+                    },
+                  ],
+                  "buttons": [
+                    {
+                      "label": "더보기",
+                      "action": "webLink",
+                      "webLinkUrl": "https://web.kangnam.ac.kr/menu/4990be9bdd4defbf92dde49a31ad1a3b.do"
+                    }
+                  ]
+                },
+              ]
+            }
+          }
+        ],
+        "quickReplies": [
+          {
+            "messageText": "홈 으로",
+            "action": "message",
+            "label": "홈"
+          },
+          {
+            "messageText": "뒤로 가기",
+            "action": "message",
+            "label": "↩"
+          }
+        ]
+      }
+    })
+  });
+});
