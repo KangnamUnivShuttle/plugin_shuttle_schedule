7d2021b4370d17ad22ca68593b8d859b6525c70a
Kim HyunWoo <stories2@naver.com>
2022-04-28 00:57:16 +0900

    asdf

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-require('dotenv').config()
-const app = express()
+require("dotenv").config();
+const app = express();
 
 // application/json
 app.use(express.json());
@@ -10,101 +10,128 @@ app.use(express.json());
 // application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-const axios = require('axios');
-const {errorResponse, carouselTemplate, 
-    listTemplate, listItem, basicCardTemplate, 
-    buttonItem, successResponse } = require('./lib');
+const axios = require("axios");
+const {
+  errorResponse,
+  carouselTemplate,
+  listTemplate,
+  listItem,
+  basicCardTemplate,
+  buttonItem,
+  successResponse,
+} = require("./lib");
 
-const {ERROR_UNEXPECTED_METHOD,
-    ERROR_UNEXPECTED_ROUTE} = require('./global');
+const { ERROR_UNEXPECTED_METHOD, ERROR_UNEXPECTED_ROUTE } = require("./global");
 
 const {
-    SHUTTLE_DATA,
-    getShuttleRouteData,
-    getShuttleStationList,
-    getNearestShuttleData,
-    getRouteNameFromKey} = require('./db')
+  SHUTTLE_DATA,
+  getShuttleRouteData,
+  getShuttleStationList,
+  getNearestShuttleData,
+  getRouteNameFromKey,
+} = require("./db");
 
+app.post("/chat", async (req, res) => {
+  if (
+    !req.body["method"] ||
+    !["route", "stations", "neartime"].includes(req.body["method"])
+  ) {
+    errorResponse.template.outputs[0].simpleText.text = ERROR_UNEXPECTED_METHOD;
+    res.send(errorResponse);
+    return;
+  }
 
-app.post('/chat', (req, res) => {
+  switch (req.body["method"]) {
+    case "route":
+      const routes = await getShuttleRouteData(req.body.dev || false);
 
-    if (!req.body['method'] || !['route', 'stations', 'neartime'].includes(req.body['method'])) {
-        errorResponse.template.outputs[0].simpleText.text = ERROR_UNEXPECTED_METHOD
-        res.send(errorResponse)
-        return
-    }
+      carouselTemplate.carousel.type = "basicCard";
+      basicCardTemplate.title = "셔틀 버스 행선지 선택";
+      basicCardTemplate.description = "이동하려는 행선지를 선택하세요";
 
-    switch(req.body['method']) {
-        case 'route':
-            const routes = getShuttleRouteData(req.body.dev || false)
+      const sortedRoutes = routes.sort((a, b) => a.order - b.order);
 
-            carouselTemplate.carousel.type = 'basicCard'
-            basicCardTemplate.title = '셔틀 버스 행선지 선택'
-            basicCardTemplate.description = '이동하려는 행선지를 선택하세요'
+      for (let i = 0; i < sortedRoutes.length; i++) {
+        buttonItem.label = sortedRoutes[i].name;
+        buttonItem.messageText = `${sortedRoutes[i].name}`;
+        basicCardTemplate.buttons.push(buttonItem);
+        if (i % 3 === 2 || i === sortedRoutes.length - 1) {
+          carouselTemplate.carousel.items.push(
+            JSON.parse(JSON.stringify(basicCardTemplate))
+          );
+          basicCardTemplate.buttons.length = 0;
+        }
+      }
+      break;
+    case "stations":
+      const stations = await getShuttleStationList(
+        req.body.route,
+        req.body.dev || false
+      );
 
-            const sortedRoutes = routes.sort((a, b) => a.order - b.order)
+      if (!stations) {
+        errorResponse.template.outputs[0].simpleText.text =
+          ERROR_UNEXPECTED_ROUTE;
+        res.send(errorResponse);
+        return;
+      }
 
-            for(let i = 0; i < sortedRoutes.length; i++) {
-                buttonItem.label = sortedRoutes[i].name
-                buttonItem.messageText = `${sortedRoutes[i].name}`
-                basicCardTemplate.buttons.push(buttonItem)
-                if (i % 3 === 2 || i === sortedRoutes.length - 1) {
-                    carouselTemplate.carousel.items.push(JSON.parse(JSON.stringify(basicCardTemplate)))
-                    basicCardTemplate.buttons.length = 0
-                }
-            }
-            break;
-        case 'stations':
-            const stations = getShuttleStationList(req.body.route, req.body.dev || false)
+      carouselTemplate.carousel.type = "listCard";
+      listTemplate.header.title = `${await getRouteNameFromKey(
+        req.body.route,
+        req.body.dev
+      )} 경로`;
 
-            if (!stations) {
-                errorResponse.template.outputs[0].simpleText.text = ERROR_UNEXPECTED_ROUTE
-                res.send(errorResponse)
-                return 
-            }
+      const sortedStations = stations.sort((a, b) => a.order - b.order);
 
-            carouselTemplate.carousel.type = 'listCard'
-            listTemplate.header.title = `${getRouteNameFromKey(req.body.route)} 경로`
-            
-            const sortedStations = stations.sort((a, b) => a.order - b.order)
+      for (let i = 0; i < sortedStations.length; i++) {
+        listItem.title = sortedStations[i].name;
+        listItem.description = sortedStations[i].descript;
+        listTemplate.items.push(JSON.parse(JSON.stringify(listItem)));
 
-            for(let i = 0; i < sortedStations.length; i ++) {
-                listItem.title = sortedStations[i].name
-                listItem.description = sortedStations[i].descript
-                listTemplate.items.push(JSON.parse(JSON.stringify(listItem)))
+        if (i % 5 === 4 || i === sortedStations.length - 1) {
+          carouselTemplate.carousel.items.push(
+            JSON.parse(JSON.stringify(listTemplate))
+          );
+          listTemplate.items.length = 0;
+        }
+      }
+      break;
+    case "neartime":
+      const neartime = await getNearestShuttleData(
+        req.body.route,
+        req.body.time,
+        req.body.dev || false
+      );
+      carouselTemplate.carousel.type = "listCard";
+      listTemplate.header.title = `${await getRouteNameFromKey(
+        req.body.route,
+        req.body.dev
+      )} 경로`;
+      // console.log('neartime', neartime)
 
-                if (i % 5 === 4 || i === sortedStations.length - 1) {
-                    carouselTemplate.carousel.items.push(JSON.parse(JSON.stringify(listTemplate)))
-                    listTemplate.items.length = 0
-                }
-            }
-            break;
-        case 'neartime':
-            const neartime = getNearestShuttleData(req.body.route, req.body.time, req.body.dev || false)
-            carouselTemplate.carousel.type = 'listCard'
-            listTemplate.header.title = `${getRouteNameFromKey(req.body.route)} 경로`
-            // console.log('neartime', neartime)
+      const sortedNearTime = neartime.sort((a, b) => a.order - b.order);
 
-            const sortedNearTime = neartime.sort((a, b) => a.order - b.order)
+      for (let i = 0; i < sortedNearTime.length; i++) {
+        listItem.title = sortedNearTime[i].name;
+        listItem.description = sortedNearTime[i].msg;
+        listTemplate.items.push(JSON.parse(JSON.stringify(listItem)));
 
-            for (let i = 0; i < sortedNearTime.length; i ++) {
-                listItem.title = sortedNearTime[i].name
-                listItem.description = sortedNearTime[i].msg
-                listTemplate.items.push(JSON.parse(JSON.stringify(listItem)))
+        if (i % 5 === 4 || i === sortedNearTime.length - 1) {
+          carouselTemplate.carousel.items.push(
+            JSON.parse(JSON.stringify(listTemplate))
+          );
+          listTemplate.items.length = 0;
+        }
+      }
 
-                if (i % 5 === 4 || i === sortedNearTime.length - 1) {
-                    carouselTemplate.carousel.items.push(JSON.parse(JSON.stringify(listTemplate)))
-                    listTemplate.items.length = 0
-                }
-            }
+      break;
+  }
 
-            break;
-    }
-
-    successResponse.template.outputs.push(carouselTemplate)
-    res.send(successResponse)
-    carouselTemplate.carousel.items.length = 0
-    successResponse.template.outputs.length = 0
-})
+  successResponse.template.outputs.push(carouselTemplate);
+  res.send(successResponse);
+  carouselTemplate.carousel.items.length = 0;
+  successResponse.template.outputs.length = 0;
+});
 
 module.exports = app;
--- src/db.js
+++ src/db.js
@@ -1,244 +1,266 @@
-const moment = require('moment');
+const moment = require("moment");
 
 const SHUTTLE_DATA = {
-    '-LXcJzZkD2g9OG50q5pt': {
-        name: '이공관 행',
+  "-LXcJzZkD2g9OG50q5pt": {
+    name: "이공관 행",
+    status: true,
+    order: 1,
+    stations: {
+      Ah7lO0GS9M: {
+        name: "기흥역 4번출구",
+        descript: "memo 1",
         status: true,
         order: 1,
-        stations: {
-            'Ah7lO0GS9M': {
-                name: '기흥역 4번출구',
-                descript: 'memo 1',
-                status: true,
-                order: 1,
-                schedule: []
-            },
-            'VOF5o1gUfn': {
-                name: '사훈 프라자 앞',
-                descript: 'memo 1',
-                status: true,
-                order: 2,
-                schedule: []
-            }
-        }
-    },
-    'LXcMXSvfscGRDynikKK': {
-        name: '기흥역 행',
+        schedule: [],
+      },
+      VOF5o1gUfn: {
+        name: "사훈 프라자 앞",
+        descript: "memo 1",
         status: true,
         order: 2,
-        stations: {
-            'zbIclI1pzD': {
-                name: '이공관 (기흥역)',
-                descript: 'memo 1',
-                status: true,
-                order: 1,
-                schedule: []
-            }
-        }
+        schedule: [],
+      },
     },
-    'LngYBAM1VGgvR7hrTYf': {
-        name: '기흥역 행',
+  },
+  LXcMXSvfscGRDynikKK: {
+    name: "기흥역 행",
+    status: true,
+    order: 2,
+    stations: {
+      zbIclI1pzD: {
+        name: "이공관 (기흥역)",
+        descript: "memo 1",
+        status: true,
+        order: 1,
+        schedule: [],
+      },
+    },
+  },
+  LngYBAM1VGgvR7hrTYf: {
+    name: "기흥역 행",
+    status: true,
+    order: 3,
+    stations: {
+      rIowOloH4l: {
+        name: "이공관 (사훈 프라자)",
+        descript: "memo 1",
+        status: true,
+        order: 1,
+        schedule: [],
+      },
+    },
+  },
+  "Not enough minerals": {
+    name: "12시 방향",
+    status: true,
+    order: 4,
+    dev: true,
+    stations: {
+      Bunker1: {
+        name: "일연벙",
+        descript: "memo 1",
+        status: true,
+        order: 1,
+        schedule: ["09:00:00", "09:30:00", "20:40:00", "23:59:59"],
+      },
+      Bunker2: {
+        name: "이연벙",
+        descript: "memo 2",
+        status: false,
+        order: 2,
+        schedule: ["00:00:00"],
+      },
+      Bunker3: {
+        name: "삼연벙",
+        descript: "memo 3",
         status: true,
         order: 3,
-        stations: {
-            'rIowOloH4l': {
-                name: '이공관 (사훈 프라자)',
-                descript: 'memo 1',
-                status: true,
-                order: 1,
-                schedule: []
-            }
-        }
-    },
-    'Not enough minerals': {
-        name: '12시 방향',
+        schedule: ["09:00:00"],
+      },
+      Bunker4: {
+        name: "사연벙",
+        descript: "memo 4",
         status: true,
         order: 4,
-        dev: true,
-        stations: {
-            'Bunker1': {
-                name: '일연벙',
-                descript: 'memo 1',
-                status: true,
-                order: 1,
-                schedule: ['09:00:00', '09:30:00', '20:40:00', '23:59:59']
-            },
-            'Bunker2': {
-                name: '이연벙',
-                descript: 'memo 2',
-                status: false,
-                order: 2,
-                schedule: ['00:00:00']
-            },
-            'Bunker3': {
-                name: '삼연벙',
-                descript: 'memo 3',
-                status: true,
-                order: 3,
-                schedule: ['09:00:00']
-            },
-            'Bunker4': {
-                name: '사연벙',
-                descript: 'memo 4',
-                status: true,
-                order: 4,
-                schedule: ['00:00:00']
-            },
-            'Bunker5': {
-                name: '오연벙',
-                descript: 'memo 5',
-                status: true,
-                order: 5,
-                schedule: []
-            }
-        }
-    },
-    'Spawn more overlords': {
-        name: '6시 방향',
-        status: false,
+        schedule: ["00:00:00"],
+      },
+      Bunker5: {
+        name: "오연벙",
+        descript: "memo 5",
+        status: true,
         order: 5,
-        dev: true,
-        stations: {
-            'Hatchery': {
-                name: '해처리',
-                descript: 'memo 5',
-                status: true,
-                order: 1,
-                schedule: []
-            }
-        }
-    }
-}
+        schedule: [],
+      },
+    },
+  },
+  "Spawn more overlords": {
+    name: "6시 방향",
+    status: false,
+    order: 5,
+    dev: true,
+    stations: {
+      Hatchery: {
+        name: "해처리",
+        descript: "memo 5",
+        status: true,
+        order: 1,
+        schedule: [],
+      },
+    },
+  },
+};
 
-const getShuttleRouteData = function (isDev = false) {
-    const result = []
-    for (let key in SHUTTLE_DATA) {
-        if (SHUTTLE_DATA[key].status && SHUTTLE_DATA[key].dev == isDev) {
-            result.push({
-                name: SHUTTLE_DATA[key].name,
-                order: SHUTTLE_DATA[key].order,
-                key
-            })
-        }
-    }
-    return result
-}
+const loadShuttleData = async function (isDev = false) {
+  if (isDev) {
+    return Promise.resolve(SHUTTLE_DATA);
+  }
+  return Promise.resolve({});
+};
 
-const getShuttleStationList = function(route, isDev = false) {
-    const result = []
-    if (!SHUTTLE_DATA.hasOwnProperty(route) || !SHUTTLE_DATA[route].stations || !SHUTTLE_DATA[route].status || SHUTTLE_DATA[route].dev !== isDev) {
-        return null
+const getShuttleRouteData = async function (isDev = false) {
+  const result = [];
+  const shuttleData = await loadShuttleData(isDev);
+  for (let key in shuttleData) {
+    if (shuttleData[key].status && shuttleData[key].dev == isDev) {
+      result.push({
+        name: shuttleData[key].name,
+        order: shuttleData[key].order,
+        key,
+      });
     }
+  }
+  return result;
+};
 
-    const stations = SHUTTLE_DATA[route].stations
+const getShuttleStationList = async function (route, isDev = false) {
+  const result = [];
+  const shuttleData = await loadShuttleData(isDev);
+  if (
+    !shuttleData.hasOwnProperty(route) ||
+    !shuttleData[route].stations ||
+    !shuttleData[route].status ||
+    shuttleData[route].dev !== isDev
+  ) {
+    return null;
+  }
 
-    for(let key in stations) {
-        if (stations[key].status) {
-            result.push({
-                name: stations[key].name,
-                order: stations[key].order,
-                schedule: stations[key].schedule,
-                descript: stations[key].descript,
-                key
-            })
-        }
+  const stations = shuttleData[route].stations;
+
+  for (let key in stations) {
+    if (stations[key].status) {
+      result.push({
+        name: stations[key].name,
+        order: stations[key].order,
+        schedule: stations[key].schedule,
+        descript: stations[key].descript,
+        key,
+      });
     }
-    return result
-}
+  }
+  return result;
+};
 
 const calcNearestTime = function (time, schedule, idx = 0) {
-    if (!/^([\d]{2}[\:]?){3}$/g.exec(time) || !schedule) {
-        return {
-            msg: 'NA',
-            idx: 0
-        }
-    }
-    if (schedule.length <= 0) {
-        return {
-            msg: '운행 정보 없음',
-            idx: 0
-        }
-    }
-
-    // console.log('calc', time, idx, schedule)
-
-    // calc nearest
-    for(let i = idx; i < schedule.length; i++) {
-        const diff = moment(schedule[i], 'HH:mm:ss').diff(moment(time, 'HH:mm:ss'))
-        // console.log('diff', schedule[i], time, diff, moment(time, 'HH:mm:ss'), moment(time, 'HH:mm:ss').diff(moment(schedule[i], 'HH:mm:ss')))
-        // console.log('range', schedule[i - 1], time, schedule[i])
-
-        if (60000 <= Math.abs(diff) && Math.abs(diff) <= 600000 && diff > 0) { // 1 ~ 10분 전
-            return {
-                msg: `${Math.floor(Math.abs(diff) / 60000)}분 후 출발`,
-                idx: i
-            }
-        } else if (0 <= diff && Math.abs(diff) < 60000) {// 잠시후
-            return {
-                msg: '잠시후 출발',
-                idx: i
-            }
-        } else if (0 < i && ((schedule[i - 1] || '00:00:00') < time && time < schedule[i])
-                        || (idx !== 0 && time < schedule[i])) { // 통상 시간 범위
-            return {
-                msg: `${idx === 0 ? '이번' : '다음'} ${schedule[i]}`,
-                idx: i
-            }
-        } else if (i === 0 && time < schedule[i]) { // 첫차 전
-            return {
-                msg: `첫차 ${schedule[i]}`,
-                idx: i
-            }
-        } else if (i === schedule.length && schedule[i] < time) { // 막차 놓침
-            return {
-                msg: `첫차 ${schedule[0]}`,
-                idx: i
-            }
-        }
-    }
+  if (!/^([\d]{2}[\:]?){3}$/g.exec(time) || !schedule) {
     return {
-        msg: idx === 0 ? '운행종료' : `첫차 ${schedule[0]}`,
-        idx: 0
+      msg: "NA",
+      idx: 0,
+    };
+  }
+  if (schedule.length <= 0) {
+    return {
+      msg: "운행 정보 없음",
+      idx: 0,
+    };
+  }
+
+  // console.log('calc', time, idx, schedule)
+
+  // calc nearest
+  for (let i = idx; i < schedule.length; i++) {
+    const diff = moment(schedule[i], "HH:mm:ss").diff(moment(time, "HH:mm:ss"));
+    // console.log('diff', schedule[i], time, diff, moment(time, 'HH:mm:ss'), moment(time, 'HH:mm:ss').diff(moment(schedule[i], 'HH:mm:ss')))
+    // console.log('range', schedule[i - 1], time, schedule[i])
+
+    if (60000 <= Math.abs(diff) && Math.abs(diff) <= 600000 && diff > 0) {
+      // 1 ~ 10분 전
+      return {
+        msg: `${Math.floor(Math.abs(diff) / 60000)}분 후 출발`,
+        idx: i,
+      };
+    } else if (0 <= diff && Math.abs(diff) < 60000) {
+      // 잠시후
+      return {
+        msg: "잠시후 출발",
+        idx: i,
+      };
+    } else if (
+      (0 < i && (schedule[i - 1] || "00:00:00") < time && time < schedule[i]) ||
+      (idx !== 0 && time < schedule[i])
+    ) {
+      // 통상 시간 범위
+      return {
+        msg: `${idx === 0 ? "이번" : "다음"} ${schedule[i]}`,
+        idx: i,
+      };
+    } else if (i === 0 && time < schedule[i]) {
+      // 첫차 전
+      return {
+        msg: `첫차 ${schedule[i]}`,
+        idx: i,
+      };
+    } else if (i === schedule.length && schedule[i] < time) {
+      // 막차 놓침
+      return {
+        msg: `첫차 ${schedule[0]}`,
+        idx: i,
+      };
     }
-}
+  }
+  return {
+    msg: idx === 0 ? "운행종료" : `첫차 ${schedule[0]}`,
+    idx: 0,
+  };
+};
 
-const calcNearestTimeSummary = function(time, schedule) {
-    const {msg: nearMsg, idx: nearIdx} = calcNearestTime(time, schedule)
-    // console.log('near', nearMsg, nearIdx)
-    const {msg: nextMsg, idx: _} = calcNearestTime(time, schedule, nearIdx + 1)
-    // console.log('next', nextMsg, _)
-    return `${nearMsg} / ${nextMsg}`
-}
+const calcNearestTimeSummary = function (time, schedule) {
+  const { msg: nearMsg, idx: nearIdx } = calcNearestTime(time, schedule);
+  // console.log('near', nearMsg, nearIdx)
+  const { msg: nextMsg, idx: _ } = calcNearestTime(time, schedule, nearIdx + 1);
+  // console.log('next', nextMsg, _)
+  return `${nearMsg} / ${nextMsg}`;
+};
 
-const getNearestShuttleData = function(route, time, isDev = false) {
-    const stations = getShuttleStationList(route, isDev)
-    // console.log(stations)
-    const result = []
-    if (!/^([\d]{2}[\:]?){3}$/g.exec(time)) {
-        return result
-    }
+const getNearestShuttleData = async function (route, time, isDev = false) {
+  const stations = await getShuttleStationList(route, isDev);
+  // console.log(stations)
+  const result = [];
+  if (!/^([\d]{2}[\:]?){3}$/g.exec(time)) {
+    return result;
+  }
 
-    stations.forEach(station => {
-        result.push({
-            name: station.name,
-            order: station.order,
-            msg: calcNearestTimeSummary(time, station.schedule)
-        })
-    })
-    return result
-}
+  stations.forEach((station) => {
+    result.push({
+      name: station.name,
+      order: station.order,
+      msg: calcNearestTimeSummary(time, station.schedule),
+    });
+  });
+  return result;
+};
 
-const getRouteNameFromKey = function(key) {
-    if (!SHUTTLE_DATA.hasOwnProperty(key)) {
-        return 'NA'
-    }
-    return SHUTTLE_DATA[key].name
-}
+const getRouteNameFromKey = async function (key, isDev = false) {
+  const shuttleData = await loadShuttleData(isDev);
+  if (!shuttleData.hasOwnProperty(key)) {
+    return "NA";
+  }
+  return shuttleData[key].name;
+};
 
 module.exports = {
-    SHUTTLE_DATA,
-    getShuttleRouteData,
-    getShuttleStationList,
-    getNearestShuttleData,
-    getRouteNameFromKey
-}
+  SHUTTLE_DATA,
+  getShuttleRouteData,
+  getShuttleStationList,
+  getNearestShuttleData,
+  getRouteNameFromKey,
+};
